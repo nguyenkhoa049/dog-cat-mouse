e3cb753dcef075ca997271693d7bc154ac8bd309
nguyenkhoa <nguyendinhkhoa049@gmail.com>
2020-05-17 12:45:46 +0700

    Sayhi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,12 @@
-function Dog () {
+var chalk = require('chalk');
+function Dog (name) {
+	this.name = name;
 	this.stomch = [];
 }
 Dog.prototype.eat = function(cat){
 	this.stomch.push(cat)
 };
+Dog.prototype.sayHi = function(){
+	console.log('Hi,My name is '+ chalk.blue(this.name));
+};
+module.exports = Dog;
